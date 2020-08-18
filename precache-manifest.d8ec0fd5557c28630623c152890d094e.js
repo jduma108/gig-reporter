a455430d52b87469da0f7c4fc61b19537c570183
jduma108 <jduma108@gmail.com>
2020-08-17 20:41:20 -0500

    Updates

---
+++ precache-manifest.d8ec0fd5557c28630623c152890d094e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5acc724a591acac3530ce36db57bfb9",
+    "url": "/gig-reporter/index.html"
+  },
+  {
+    "revision": "ff36904c5dbdb8083364",
+    "url": "/gig-reporter/static/css/2.67227d5c.chunk.css"
+  },
+  {
+    "revision": "bafa93ac33b57627a7c6",
+    "url": "/gig-reporter/static/css/main.fb40f603.chunk.css"
+  },
+  {
+    "revision": "ff36904c5dbdb8083364",
+    "url": "/gig-reporter/static/js/2.93d1253c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gig-reporter/static/js/2.93d1253c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bafa93ac33b57627a7c6",
+    "url": "/gig-reporter/static/js/main.232105f1.chunk.js"
+  },
+  {
+    "revision": "90d7e518e273af7fa19a",
+    "url": "/gig-reporter/static/js/runtime-main.5714acb9.js"
+  },
+  {
+    "revision": "4b84f206df6ac0c04f51d2754cb46221",
+    "url": "/gig-reporter/static/media/ajr.4b84f206.jpg"
+  },
+  {
+    "revision": "13661d576a2adf964d0a1563ab146590",
+    "url": "/gig-reporter/static/media/background.13661d57.png"
+  },
+  {
+    "revision": "f044747fdaef49cc5ce1cc73c0cfe099",
+    "url": "/gig-reporter/static/media/bts.f044747f.jpg"
+  },
+  {
+    "revision": "2cf67be0cd205dbde44252713b6815d1",
+    "url": "/gig-reporter/static/media/halsey.2cf67be0.jpg"
+  },
+  {
+    "revision": "40d7814874341f49085d19d7091ccd70",
+    "url": "/gig-reporter/static/media/hellaMegaTour.40d78148.jpg"
+  },
+  {
+    "revision": "1cc3500aca54f744ae41fd0ad38eb100",
+    "url": "/gig-reporter/static/media/jonBellion.1cc3500a.jpg"
+  },
+  {
+    "revision": "b88ebacb8d51cb1e139a9c1e2ed25ef3",
+    "url": "/gig-reporter/static/media/muse.b88ebacb.jpg"
+  }
+]);
